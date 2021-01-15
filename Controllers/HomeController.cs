ee272529c31421528a6800c7e7a02db60fb6c97f
GabrielJTavares <gabriel-tavares1996@outlook.com>
2021-01-15 18:09:39 -0300

    Envio de email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LojaVirtual.Models;
+using LojaVirtual.Libraries.Email;
 
 namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        private object contatoEmail;
+
         public IActionResult Index()
         {
             return View();
@@ -20,10 +24,13 @@ namespace LojaVirtual.Controllers
 
         public IActionResult ContatoAcao()
         {
-            string nome = HttpContext.Request.Form["nome"];
-            string email = HttpContext.Request.Form["email"];
-            string texto = HttpContext.Request.Form["texto"];
-            return new ContentResult() {Content= string.Format("Dados recebidos com sucesso!<br/> Nome: {0}<br/> Email:{1}<br/> Texto:{2}",nome,email,texto), ContentType="text/html" };
+            Contato contato = new Contato();
+            contato.Nome = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto = HttpContext.Request.Form["texto"];
+
+            ContatoEmail.EnviarContatoPorEmail(contato);
+            return new ContentResult() {Content= string.Format("Dados recebidos com sucesso!<br/> Nome: {0}<br/> Email:{1}<br/> Texto:{2}",contato.Nome,contato.Email,contato.Texto), ContentType="text/html" };
         }
 
         public IActionResult Login()
---
+++ Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("gabriel-tavares78@uni9.edu.br", "bass71163");
+            smtp.EnableSsl = true;
+
+            string corpoMsg = string.Format("<h2> Contato - Careca's Store</h2>" +
+            "<b>Nome: </b> {0} <br />" +
+            "<b>E-mail: </b> {1} <br />" +
+            "<b>Texto: </b> {2} <br />" +
+            "Email enviado automaticamente pelo site Careca's Store",
+            contato.Nome, contato.Email, contato.Texto);
+
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("gabriel-tavares78@uni9.edu.br");
+            mensagem.To.Add(contato.Email);
+            mensagem.Subject = "Contato - Careca's Store";
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            smtp.Send(mensagem);
+
+
+         
+        }
+    }
+}
